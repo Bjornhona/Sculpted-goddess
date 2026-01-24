c6fb5b281f9d4f0ddada4d5140dd18d8f29183e6
Bjornhona <asaeri3@hotmail.com>
2026-01-24 13:45:44 +0100

    desired weight made responsive

--- components/macro-calculator/desired-weight/DesiredWeight.tsx
+++ components/macro-calculator/desired-weight/DesiredWeight.tsx
@@ -18,102 +18,106 @@ const DesiredWeight = ({ onSave }: DesiredWeightProps) => {
 
   return (
     <div className={styles.manageWeightMain}>
-      <div
-        className={`${styles.mainHeaderText} ${styles.mainTwoHeaderText}`}
-        id="desiredWeightContainer"
-      >
-        <h2>
-          <span>Set your</span> goal
-        </h2>
-        <h3>To calculate your macronutrient ratio</h3>
-      </div>
-      <form
-        className={styles.mainContent}
-        id="macronutrientRatio"
-        action={saveUserData}
-      >
-        {/* HIDDEN INPUTS */}
-        <input type="hidden" name="gender" value={gender ?? ""} />
-        <input type="hidden" name="weight" value={weight ?? ""} />
-        <input type="hidden" name="height" value={height ?? ""} />
-        <input type="hidden" name="age" value={age ?? ""} />
-        <input type="hidden" name="activity" value={activity ?? ""} />
-        <h3>
-          Congratulations, your total number of calories burned in a day is{" "}
-          <span>{recommendedCalIntake}</span> cal!
-        </h3>
-
-        <div className={styles.actionBox}>
+      <div className={styles.mainContainer}>
+        <div
+          className={`${styles.mainHeaderText} ${styles.mainTwoHeaderText}`}
+          id="desiredWeightContainer"
+        >
+          <h2>
+            <span>Set your</span> goal
+          </h2>
+          <h3>To calculate your macronutrient ratio</h3>
+        </div>
+        <form
+          className={styles.mainContent}
+          id="macronutrientRatio"
+          action={saveUserData}
+        >
+          {/* HIDDEN INPUTS */}
+          <input type="hidden" name="gender" value={gender ?? ""} />
+          <input type="hidden" name="weight" value={weight ?? ""} />
+          <input type="hidden" name="height" value={height ?? ""} />
+          <input type="hidden" name="age" value={age ?? ""} />
+          <input type="hidden" name="activity" value={activity ?? ""} />
           <h3>
-            <span>Desired action</span>
+            Congratulations, your total number of calories burned in a day is{" "}
+            <span>{recommendedCalIntake}</span> cal!
           </h3>
-          <div className={styles.actionContainer}>
-            <div>
-              <input
-                className={styles.gainInput}
-                type="radio"
-                id="gain"
-                name="action"
-                checked={action === "gain"}
-                onChange={() => setAction("gain")}
-                value="gain"
-                required
-              />
-              <label className={styles.gain} htmlFor="gain">
-                <FaDumbbell />
-                <div className={styles.textBox}>
-                  <h3>I want to</h3>
-                  <h2>gain muscle</h2>
-                </div>
-              </label>
-            </div>
-            <div>
-              <input
-                className={styles.loseInput}
-                type="radio"
-                id="lose"
-                name="action"
-                value="lose"
-                checked={action === "lose"}
-                onChange={() => setAction("lose")}
-              />
-              <label className={styles.lose} htmlFor="lose">
-                <FaWeight />
-                <div className={styles.textBox}>
-                  <h3>I want to</h3>
-                  <h2>lose weight</h2>
-                </div>
-              </label>
-            </div>
-          </div>
-        </div>
 
-        <div className={styles.desiredWeightContainer}>
-          <div className={styles.weight}>
+          <div className={styles.actionBox}>
             <h3>
-              <span>Desired weight</span>
+              <span>Desired action</span>
             </h3>
-            <input
-              type="number"
-              id="desiredWeight"
-              name="desiredWeight"
-              value={desiredWeight ?? ""}
-              onChange={(e) => setDesiredWeight(Number(e.target.value))}
-              placeholder="0"
-              min={action === "gain" ? weight ?? 0 : 0}
-              max={action === "gain" ? 300 : weight ?? 300}
-              required
-            />
-            <h3 className={styles.unit}>kg</h3>
+            <div className={styles.actionContainer}>
+              <div>
+                <input
+                  className={styles.gainInput}
+                  type="radio"
+                  id="gain"
+                  name="action"
+                  checked={action === "gain"}
+                  onChange={() => setAction("gain")}
+                  value="gain"
+                  required
+                />
+                <label className={styles.gain} htmlFor="gain">
+                  <FaDumbbell />
+                  <div className={styles.textBox}>
+                    <h3>I want to</h3>
+                    <h2>gain muscle</h2>
+                  </div>
+                </label>
+              </div>
+              <div>
+                <input
+                  className={styles.loseInput}
+                  type="radio"
+                  id="lose"
+                  name="action"
+                  value="lose"
+                  checked={action === "lose"}
+                  onChange={() => setAction("lose")}
+                />
+                <label className={styles.lose} htmlFor="lose">
+                  <FaWeight />
+                  <div className={styles.textBox}>
+                    <h3>I want to</h3>
+                    <h2>lose weight</h2>
+                  </div>
+                </label>
+              </div>
+            </div>
           </div>
-        </div>
 
-        <div className={styles.calculateMakro}>
-          <button disabled={!action || !desiredWeight} type="submit" id="macro-button">
-            Show my makro needs
-          </button>
-        </div>
-      </form>
+          <div className={styles.desiredWeightContainer}>
+            <div className={styles.weight}>
+              <h3>
+                <span>Desired weight</span>
+              </h3>
+              <div className={styles.inputContainer}>
+                <input
+                  type="number"
+                  id="desiredWeight"
+                  name="desiredWeight"
+                  value={desiredWeight ?? ""}
+                  onChange={(e) => setDesiredWeight(Number(e.target.value))}
+                  placeholder="0"
+                  min={action === "gain" ? weight ?? 0 : 0}
+                  max={action === "gain" ? 300 : weight ?? 300}
+                  required
+                />
+                <h3 className={styles.unit}>kg</h3>
+              </div>
+            </div>
+          </div>
+
+          <div className={styles.calculateMakro}>
+            <button disabled={!action || !desiredWeight} type="submit" id="macro-button">
+              Show my makro needs
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
--- components/macro-calculator/dietary-needs/DietaryNeeds.tsx
+++ components/macro-calculator/dietary-needs/DietaryNeeds.tsx
@@ -33,166 +33,168 @@ const DietaryNeeds = ({ onSave }: Props) => {
 
   return (
     <div className={styles.manageWeightMain}>
-      <div className={styles.mainHeaderText} id="dietaryNeedsContainer">
-        <h2>
-          <span>Introduce</span> your data
-        </h2>
-        <h3>To calculate your dietary needs</h3>
-      </div>
-
-      <form className={styles.mainContent} action={saveUserData}>
-        {/* GENDER */}
-        <div className={styles.genderBox}>
-          <h3>
-            <span>Gender</span>
-          </h3>
-          <div className={styles.genderContainer}>
-            <div>
-              <input
-                className={styles.maleInput}
-                type="radio"
-                id="male"
-                name="gender"
-                value={5}
-                checked={gender === 5}
-                onChange={(e) => setGender(Number(e.target.value))}
-                required
-              />
-              <label className={styles.male} htmlFor="male">
-                <FaMale />
-                <div className={styles.textBox}>
-                  <h3>I am</h3>
-                  <h2>male</h2>
-                </div>
-              </label>
-            </div>
-
-            <div>
-              <input
-                className={styles.femaleInput}
-                type="radio"
-                id="female"
-                name="gender"
-                value={-161}
-                checked={gender === -161}
-                onChange={(e) => setGender(Number(e.target.value))}
-              />
-              <label className={styles.female} htmlFor="female">
-                <FaFemale />
-                <div className={styles.textBox}>
-                  <h3>I am</h3>
-                  <h2>female</h2>
-                </div>
-              </label>
-            </div>
-          </div>
+      <div className={styles.mainContainer}>
+        <div className={styles.mainHeaderText} id="dietaryNeedsContainer">
+          <h2>
+            <span>Introduce</span> your data
+          </h2>
+          <h3>To calculate your dietary needs</h3>
         </div>
 
-        {/* WEIGHT / HEIGHT / AGE */}
-        <div className={styles.weightHeightAgeContainer}>
-          <div className={styles.weight}>
+        <form className={styles.mainContent} action={saveUserData}>
+          {/* GENDER */}
+          <div className={styles.genderBox}>
             <h3>
-              <span>Weight</span>
+              <span>Gender</span>
             </h3>
-            <div className={styles.inputContainer}>
-              <input
-                type="number"
-                name="weight"
-                placeholder="0"
-                min="0"
-                max="300"
-                value={weight ?? ""}
-                onChange={(e) => setWeight(parseFloat(e.target.value))}
-                required
-              />
-              <h3 className={styles.unit}>kg</h3>
-            </div>
-          </div>
-
-          <div className={styles.height}>
-            <h3>
-              <span>Height</span>
-            </h3>
-            <div className={styles.inputContainer}>
-              <input
-                type="number"
-                name="height"
-                placeholder="0"
-                min="0"
-                max="300"
-                value={height ?? ""}
-                onChange={(e) => setHeight(parseFloat(e.target.value))}
-                required
-              />
-              <h3 className={styles.unit}>cm</h3>
-            </div>
-          </div>
-
-          <div className={styles.age}>
-            <h3>
-              <span>Age</span>
-            </h3>
-            <div className={styles.inputContainer}>
-              <input
-                type="number"
-                name="age"
-                placeholder="0"
-                min="0"
-                max="130"
-                value={age ?? ""}
-                onChange={(e) => setAge(Number(e.target.value))}
-                required
-              />
-              <h3 className={styles.unit}>years</h3>
-            </div>
-          </div>
-        </div>
-
-        {/* ACTIVITY */}
-        <div className={styles.activityLevel}>
-          <h3>
-            <span>Activity level</span>
-          </h3>
-          <div className={styles.activityOptionsContainer}>
-            {[
-              { id: "sedentary", val: 1.2 },
-              { id: "lightly-active", val: 1.375 },
-              { id: "moderately-active", val: 1.55 },
-              { id: "very-active", val: 1.725 },
-              { id: "extra-active", val: 1.9 },
-            ].map((item) => (
-              <label
-                key={item.id}
-                className={styles.container}
-                htmlFor={item.id}
-              >
+            <div className={styles.genderContainer}>
+              <div>
                 <input
+                  className={styles.maleInput}
                   type="radio"
-                  id={item.id}
-                  name="activity"
-                  value={item.val}
-                  checked={activity === item.val}
-                  onChange={(e) => setActivity(Number(e.target.value))}
+                  id="male"
+                  name="gender"
+                  value={5}
+                  checked={gender === 5}
+                  onChange={(e) => setGender(Number(e.target.value))}
                   required
                 />
-                <span className={styles.checkmark}></span>
-              </label>
-            ))}
+                <label className={styles.male} htmlFor="male">
+                  <FaMale />
+                  <div className={styles.textBox}>
+                    <h3>I am</h3>
+                    <h2>male</h2>
+                  </div>
+                </label>
+              </div>
+
+              <div>
+                <input
+                  className={styles.femaleInput}
+                  type="radio"
+                  id="female"
+                  name="gender"
+                  value={-161}
+                  checked={gender === -161}
+                  onChange={(e) => setGender(Number(e.target.value))}
+                />
+                <label className={styles.female} htmlFor="female">
+                  <FaFemale />
+                  <div className={styles.textBox}>
+                    <h3>I am</h3>
+                    <h2>female</h2>
+                  </div>
+                </label>
+              </div>
+            </div>
           </div>
 
-          <div className={styles.labelText}>
-            <h4>sedentary</h4>
-            <h4>extra active</h4>
-          </div>
-        </div>
+          {/* WEIGHT / HEIGHT / AGE */}
+          <div className={styles.weightHeightAgeContainer}>
+            <div className={styles.weight}>
+              <h3>
+                <span>Weight</span>
+              </h3>
+              <div className={styles.inputContainer}>
+                <input
+                  type="number"
+                  name="weight"
+                  placeholder="0"
+                  min="0"
+                  max="300"
+                  value={weight ?? ""}
+                  onChange={(e) => setWeight(parseFloat(e.target.value))}
+                  required
+                />
+                <h3 className={styles.unit}>kg</h3>
+              </div>
+            </div>
 
-        {/* SUBMIT (Disabled until valid) */}
-        <div className={styles.calculateCalories}>
-          <button disabled={!isFormValid} type="submit">
-            Calculate calorie intake
-          </button>
-        </div>
-      </form>
+            <div className={styles.height}>
+              <h3>
+                <span>Height</span>
+              </h3>
+              <div className={styles.inputContainer}>
+                <input
+                  type="number"
+                  name="height"
+                  placeholder="0"
+                  min="0"
+                  max="300"
+                  value={height ?? ""}
+                  onChange={(e) => setHeight(parseFloat(e.target.value))}
+                  required
+                />
+                <h3 className={styles.unit}>cm</h3>
+              </div>
+            </div>
+
+            <div className={styles.age}>
+              <h3>
+                <span>Age</span>
+              </h3>
+              <div className={styles.inputContainer}>
+                <input
+                  type="number"
+                  name="age"
+                  placeholder="0"
+                  min="0"
+                  max="130"
+                  value={age ?? ""}
+                  onChange={(e) => setAge(Number(e.target.value))}
+                  required
+                />
+                <h3 className={styles.unit}>years</h3>
+              </div>
+            </div>
+          </div>
+
+          {/* ACTIVITY */}
+          <div className={styles.activityLevel}>
+            <h3>
+              <span>Activity level</span>
+            </h3>
+            <div className={styles.activityOptionsContainer}>
+              {[
+                { id: "sedentary", val: 1.2 },
+                { id: "lightly-active", val: 1.375 },
+                { id: "moderately-active", val: 1.55 },
+                { id: "very-active", val: 1.725 },
+                { id: "extra-active", val: 1.9 },
+              ].map((item) => (
+                <label
+                  key={item.id}
+                  className={styles.container}
+                  htmlFor={item.id}
+                >
+                  <input
+                    type="radio"
+                    id={item.id}
+                    name="activity"
+                    value={item.val}
+                    checked={activity === item.val}
+                    onChange={(e) => setActivity(Number(e.target.value))}
+                    required
+                  />
+                  <span className={styles.checkmark}></span>
+                </label>
+              ))}
+            </div>
+
+            <div className={styles.labelText}>
+              <h4>sedentary</h4>
+              <h4>extra active</h4>
+            </div>
+          </div>
+
+          {/* SUBMIT (Disabled until valid) */}
+          <div className={styles.calculateCalories}>
+            <button disabled={!isFormValid} type="submit">
+              Calculate calorie intake
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
